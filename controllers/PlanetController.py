7a264331827aa03c26bbbbb2adef16bb785157f0
Alexander <automaalex@hotmail.com>
2019-12-12 02:35:16 -0300

    Add PUT in PlanetsController, add arg parsing

--- controllers/PlanetController.py
+++ controllers/PlanetController.py
@@ -1,6 +1,13 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.Planet import Planet
 from flask import jsonify
+
+parser = reqparse.RequestParser()
+# Add current model attributes to parser
+for attr in Planet.__dict__.keys():
+    if attr.startswith('__'):
+        continue
+    parser.add_argument(attr)
 
 class PlanetController(Resource):
     def __planets(self, id=None):
@@ -10,15 +17,26 @@ class PlanetController(Resource):
         else:
             return Planet.objects.all()
 
+    def __body(self):
+        """ Get body args """
+        args = parser.parse_args()
+        return args
+
     # GET: /api/planeta/:?id
     def get(self, id=None):
         planet_list = self.__planets(id=id)
         if planet_list:
             return { "data": planet_list.to_json() }
 
-    # PUT: /api/planeta
-    def put(self):
-        pass
+    # PUT: /api/planeta/:id
+    def put(self, id):
+        planet = self.__planets(id=id)
+        body = self.__body()
+        planet.name = body['name']
+        planet.climate = body['climate']
+        planet.terrain = body['terrain']
+        # TODO: ADD API request to update "in_movie"
+        planet.save()
 
     # POST: /api/planeta
     def post(self):
--- models/Planet.py
+++ models/Planet.py
@@ -1,6 +1,7 @@
 import mongoengine
 
 class Planet(mongoengine.Document):
+    exclude = ['id', 'in_movie'] # Delete the need from body requests
     id = mongoengine.IntField(primary_key=True)
     name = mongoengine.StringField(max_length=50, required=True)
     climate = mongoengine.StringField(max_length=50, required=True)
