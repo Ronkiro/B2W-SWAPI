ac99a487bea80e3539bc85be98493ab0fbc0f07c
Alexander Santos <dev_alex@outlook.com>
2019-12-12 23:42:33 -0300

    Update requirements.txt, update swapi test

--- test_swapi.py
+++ test_swapi.py
@@ -1,30 +1,39 @@
 import unittest
 from resources.SWAPI import SWAPI
 
-class SWAPITestCase(unittest.TestCase):
+class TestSWAPI(unittest.TestCase):
     def setUp(self):
         self.api = SWAPI()
         self.DAGOBAH_COUNT = 3
 
-    def test_planets(self):
+    def test_if_planets_are_list(self):
         self.assertIsInstance(self.api.planets(), list)
 
-    def test_find_planet(self):
+    def test_if_find_planet_instances_are_correct(self):
         self.assertIsInstance(self.api.find_planet('Dagobah'), dict)
         self.assertIsInstance(self.api.find_planet('dagobah'), dict)
         self.assertIsNone(self.api.find_planet('MIDAWDIWJAD'))
+    
+    def test_if_find_planet_handles_incorrect_usage(self):
         with self.assertRaises(TypeError):
             self.api.find_planet(1)
             self.api.find_planet([])
             self.api.find_planet({})
             
-    def test_movie_count(self):
+    def test_if_movie_count_is_correct(self):
+        self.assertEqual(self.api.get_movie_count('MIDAWDIWJAD'), 0)
+        self.assertEqual(self.api.get_movie_count('Dagobah'), 3)
+        self.assertEqual(self.api.get_movie_count('dagobah'), 3)
+
+    def test_if_movie_count_instances_are_correct(self):
         self.assertIsInstance(self.api.get_movie_count('Dagobah'), int)
         self.assertIsInstance(self.api.get_movie_count('dagobah'), int)
-        self.assertEquals(self.api.get_movie_count('MIDAWDIWJAD'), 0)
+    
+    def test_if_movie_count_handles_incorrect_usage(self):
         with self.assertRaises(TypeError):
             self.api.get_movie_count(1)
             self.api.get_movie_count([])
             self.api.get_movie_count({})
-        self.assertEquals(self.api.get_movie_count('Dagobah'), 3)
-        self.assertEquals(self.api.get_movie_count('dagobah'), 3)
+
+if __name__ == "__main__":
+    unittest.main()
